42088005a9dcd623c98b6aef1a7c47d4eceb0dc6
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-12-04 20:48:09 +0700

    feat: setup & add database models to prisma schema

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const PORT = process.env.PORT;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+export const prisma = new PrismaClient();
---
+++ src/lib/jwt.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { TokenExpiredError, verify } from "jsonwebtoken";
+import { JWT_SECRET_KEY } from "../config";
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send({
+      message: "authentication failed, token missing",
+    });
+
+    return;
+  }
+
+  verify(token, JWT_SECRET_KEY!, (err, payload) => {
+    if (err) {
+      if (err instanceof TokenExpiredError) {
+        return res.status(401).send({ message: "Token expired" });
+      } else {
+        return res.status(401).send({ message: "invalid token" });
+      }
+    }
+
+    res.locals.user = payload;
+
+    next();
+  });
+};
