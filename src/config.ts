8fae81f532cb0ad904a0f29073052af7b9f439d9
mishbahulmunir96 <157870568+mishbahulmunir96@users.noreply.github.com>
2024-12-09 22:09:06 +0700

    feat: registerpage (#2)

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const PORT = process.env.PORT;
-export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-export const prisma = new PrismaClient();
--- src/routes/sample.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getSamplesController } from "../controllers/sample.controller";
-
-const router = Router();
-
-router.get("/", getSamplesController);
-
-export default router;
