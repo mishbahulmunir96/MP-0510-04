1d7458a753a2d750ae4cbfc69acccbe7b4c720de
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-12-10 20:35:44 +0700

    feat: login fungtionality

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { validateRegister } from "../validators/auth.validator";
-import { registerController } from "../controllers/auth.controller";
+import { validateLogin, validateRegister } from "../validators/auth.validator";
+import {
+  loginController,
+  registerController,
+} from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/register", validateRegister, registerController);
+router.post("/login", validateLogin, loginController);
 
 export default router;
