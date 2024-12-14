34e50d008e5142c50354455f0d31c71bba3be2d0
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-12-14 19:36:01 +0700

    feat: update generate referral code

--- src/services/auth/register.service.ts
+++ src/services/auth/register.service.ts
@@ -1,31 +1,19 @@
 import { Role, User } from "@prisma/client";
 import { hashPassword } from "../../lib/argon";
 import { prisma } from "../../lib/prisma";
-
-async function generateUniqueReferralCode(): Promise<string> {
-  const min = 100000; // Rentang minimum
-  const max = 999999; // Rentang maksimum
-  let code: string;
-
-  while (true) {
-    // Menghasilkan kode acak
-    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    code = randomNumber.toString(); // Konversi ke string
-
-    // Cek apakah kode referral sudah ada
-    const existingCode = await prisma.user.findFirst({
-      where: { referralCode: code },
-    });
-
-    if (!existingCode) break; // Jika tidak ada, kode unik
-  }
-
-  return code; // Kode referral yang unik dalam format string
-}
+import { generateReferralCode } from "../../lib/generateReferralcode";
 
 export const registerService = async (body: User) => {
   try {
-    const { firstName, lastName, email, phoneNumber, role, password } = body;
+    const {
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      role,
+      password,
+      referralCode,
+    } = body;
 
     const existingUser = await prisma.user.findFirst({
       where: { email },
@@ -35,8 +23,19 @@ export const registerService = async (body: User) => {
       throw new Error("Email already exist");
     }
 
-    const referralCode = await generateUniqueReferralCode();
     const hashedPassword = await hashPassword(password);
+
+    const newReferralcode = await generateReferralCode();
+
+    let referredBy = null;
+    if (referralCode) {
+      const referringUser = await prisma.user.findFirst({
+        where: { referralCode },
+      });
+      if (referringUser) {
+        referredBy = referringUser?.id;
+      }
+    }
 
     return await prisma.user.create({
       data: {
@@ -46,7 +45,8 @@ export const registerService = async (body: User) => {
         phoneNumber,
         role: role ?? Role.USER,
         password: hashedPassword,
-        referralCode,
+        referralCode: newReferralcode,
+        referredBy,
       },
     });
   } catch (error) {
