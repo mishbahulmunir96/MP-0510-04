a2d440b5862f4802b8554b867fb071ba4b7cfd1b
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-12-14 22:35:17 +0700

    feat: coupon & point referral

--- src/services/auth/register.service.ts
+++ src/services/auth/register.service.ts
@@ -2,6 +2,8 @@ import { Role, User } from "@prisma/client";
 import { hashPassword } from "../../lib/argon";
 import { prisma } from "../../lib/prisma";
 import { generateReferralCode } from "../../lib/generateReferralcode";
+import { generateCouponCode } from "../../lib/generateCouponCode";
+import { addDays } from "date-fns";
 
 export const registerService = async (body: User) => {
   try {
@@ -34,10 +36,21 @@ export const registerService = async (body: User) => {
       });
       if (referringUser) {
         referredBy = referringUser?.id;
+
+        // point
+        await prisma.user.update({
+          where: { id: referredBy },
+          data: {
+            point: {
+              increment: 10000,
+            },
+            pointExpiredDate: addDays(new Date(), 90),
+          },
+        });
       }
     }
 
-    return await prisma.user.create({
+    const newUser = await prisma.user.create({
       data: {
         firstName,
         lastName,
@@ -47,8 +60,27 @@ export const registerService = async (body: User) => {
         password: hashedPassword,
         referralCode: newReferralcode,
         referredBy,
+        pointExpiredDate: referredBy ? addDays(new Date(), 90) : null,
       },
     });
+
+    if (referredBy) {
+      const newCouponcode = await generateCouponCode();
+
+      // coupon
+      await prisma.coupon.create({
+        data: {
+          userId: newUser.id,
+          couponCode: newCouponcode,
+          value: 10,
+          isUsed: false,
+          createdAt: new Date(),
+          expiredAt: addDays(new Date(), 90),
+        },
+      });
+    }
+
+    return newUser;
   } catch (error) {
     throw error;
   }
