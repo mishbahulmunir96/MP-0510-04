a02d1bd10b0394aac7f86844de35aab4e23a4655
Muhammad Ahdan Aufa <156409996+Ahdan24@users.noreply.github.com>
2024-12-15 21:13:27 +0700

    feat: create-event (#7)

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -1,23 +1,42 @@
 import { NextFunction, Request, Response } from "express";
 import { getEventsService } from "../services/event/get-events.service";
+import { createEventService } from "../services/event/create-event.service";
 
 export const getEventsController = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const query = {
-        take: parseInt(req.query.take as string) || 3,
-        page: parseInt(req.query.page as string) || 1,
-        sortBy: (req.query.sortBy as string) || "createdAt",
-        sortOrder: (req.query.sortOrder as string) || "desc",
-        search: (req.query.search as string) || "",
-      };
-      const result = await getEventsService(query);
-      res.status(200).send(result);
-    } catch (error) {
-      next(error);
-    }
-  };
-  
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const query = {
+      take: parseInt(req.query.take as string) || 3,
+      page: parseInt(req.query.page as string) || 1,
+      sortBy: (req.query.sortBy as string) || "createdAt",
+      sortOrder: (req.query.sortOrder as string) || "desc",
+      search: (req.query.search as string) || "",
+    };
+    const result = await getEventsService(query);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createEventController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+    const result = await createEventService(
+      req.body,
+      files.thumbnail?.[0],
+      res.locals.user.id
+    );
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
