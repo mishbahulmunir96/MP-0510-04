edfd8e74fe6ea3e91af1100f961e962002b86a65
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-12-23 11:03:28 +0700

    chores: handling error if no event (getEventsByUser) & including event on get vouchers

--- src/services/voucher/get-vouchers.service.ts
+++ src/services/voucher/get-vouchers.service.ts
@@ -4,6 +4,7 @@ export const getVouchersService = async (userId: number) => {
   try {
     return await prisma.voucher.findMany({
       where: { userId: userId },
+      include: { event: true },
     });
   } catch (error) {
     throw error;
