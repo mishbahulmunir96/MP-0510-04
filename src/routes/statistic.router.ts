bed1dcc8731c2160c4b6ee848ae9e8e6c8c629ac
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2025-01-03 14:08:20 +0700

    chores: add role protect to some routes

--- src/routes/statistic.router.ts
+++ src/routes/statistic.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyToken } from "../lib/jwt";
 import { getEventsStatisticsController } from "../controllers/statistic.controller";
+import { checkUserRole } from "../lib/checkUserRole";
 
 const router = Router();
 
-router.get("/", verifyToken, getEventsStatisticsController);
+router.get("/", verifyToken, checkUserRole, getEventsStatisticsController);
 
 export default router;
