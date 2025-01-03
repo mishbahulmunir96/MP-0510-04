c5a44c170baf81db0cfa18fa1b241a4a507a7f0a
mishbahulmunir96 <157870568+mishbahulmunir96@users.noreply.github.com>
2025-01-03 14:33:38 +0700

    chore: uncomment directUrl (#32)

--- dist/routes/statistic.router.js
+++ dist/routes/statistic.router.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const jwt_1 = require("../lib/jwt");
 const statistic_controller_1 = require("../controllers/statistic.controller");
+const checkUserRole_1 = require("../lib/checkUserRole");
 const router = (0, express_1.Router)();
-router.get("/", jwt_1.verifyToken, statistic_controller_1.getEventsStatisticsController);
+router.get("/", jwt_1.verifyToken, checkUserRole_1.checkUserRole, statistic_controller_1.getEventsStatisticsController);
 exports.default = router;
